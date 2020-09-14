f5afb447bf7141035397dc33ec8cb5762b686e80
Artur Drozdov <artur.drozdov11@gmail.com>
2020-09-14 11:52:03 +0300

    add task formula nod

---
+++ src/com/drozdov/taskFormulaNOD/FormulaNod.java
@@ -0,0 +1,32 @@
+package com.drozdov.taskFormulaNOD;
+
+import java.util.Scanner;
+
+public class FormulaNod {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Введите 2 целых числа для нахождения наибольшего общего делителя.Ввод:");
+        int a=in.nextInt();
+        int b=in.nextInt();
+                        //остаток от дел.
+        System.out.println(nod(a,b));
+
+
+       // System.out.println(a+""+b+""+c);
+        // System.out.println(c);
+    }
+
+    static int nod(int a,int b){
+
+
+         while (b!=0) {
+           int c;
+        if (a < b)
+           c = b % a;
+        else c = a % b;
+        a = b;
+        b = c;
+             }
+        return a;
+    }
+}
